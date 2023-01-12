da86f9fd6bd8d62eae7e0454f55848d9ca6d1ef8
goodness <goodyc474@gmail.com>
2023-01-12 22:11:06 +0100

    add form component

---
+++ src/components/common/FetchRecipie.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const FetchRecipe = () => {
+  const key = " 29d587081cd94aba8413d56404bfcead";
+  const appId = "ef6df93e";
+
+  return <h2>hello</h2>;
+};
+
+export default FetchRecipe;
