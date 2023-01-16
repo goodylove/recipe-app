a441a5124f99c093d09ca881651f2e4ccb740b95
goodness <goodyc474@gmail.com>
2023-01-16 20:52:42 +0100

    completed mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import LogoImage from "./assets/Ellipse 10.png";
 
 import "../styles/Nav.css";
 
 const NavBar = () => {
+  const [openLink, setOpenLink] = useState(false);
+
+  const handleNavBar = () => {
+    setOpenLink(!openLink);
+  };
   return (
     <div className="nav">
-      <div className="left-side">
+      <div className="left-side" id={openLink ? "open" : "close"}>
         <img src={LogoImage} alt="logo" id="img" />
         <div className="hidden-link">
           <ul>
@@ -30,7 +35,7 @@ const NavBar = () => {
           </li>
         </ul>
       </div>
-      <div className="bars-icon">
+      <div className="bars-icon" onClick={handleNavBar}>
         <i className="fa fa-bars"></i>
       </div>
     </div>
