f06678b7894bbd2ae27e940cb3cbe37f4e27847b
goodness <goodyc474@gmail.com>
2023-01-16 20:23:10 +0100

    added loader to fectchRecipe component

--- src/components/common/FetchRecipie.jsx
+++ src/components/common/FetchRecipie.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+
+import Loader from "./../Loader";
 import Recipe from "../Recipe";
 import "./FetchRecipe.css";
 
 const FetchRecipe = () => {
   const [chicken, setChicken] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [pastry, setPastry] = useState([]);
   const [pasta, setPasta] = useState([]);
   const [salad, setSalad] = useState([]);
@@ -21,6 +24,7 @@ const FetchRecipe = () => {
   }, []);
 
   const getRecipes = async () => {
+    setLoading(true);
     const response = await Promise.all([
       fetch(appApi),
       fetch(appApi1),
@@ -33,6 +37,7 @@ const FetchRecipe = () => {
     const data3 = await response[2].json();
     const data4 = await response[3].json();
     const data5 = await response[4].json();
+    setLoading(false);
 
     setChicken(data1.hits);
     setPastry(data2.hits);
@@ -42,68 +47,74 @@ const FetchRecipe = () => {
   };
 
   return (
-    <div className="allRecipe">
-      <div className="all-item-wrapper">
-        <span>BUGER</span>
-        <div className="items">
-          {buger.map((recip, index) => (
-            <Recipe
-              key={index}
-              label={recip.recipe.label}
-              image={recip.recipe.image}
-            />
-          ))}
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="allRecipe">
+          <div className="all-item-wrapper">
+            <span>BUGER</span>
+            <div className="items">
+              {buger.map((recip, index) => (
+                <Recipe
+                  key={index}
+                  label={recip.recipe.label}
+                  image={recip.recipe.image}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="all-item-wrapper">
+            <span>Chicken</span>
+            <div className="items">
+              {chicken.map((recip, index) => (
+                <Recipe
+                  key={index}
+                  label={recip.recipe.label}
+                  image={recip.recipe.image}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="all-item-wrapper">
+            <span>PASTRY</span>
+            <div className="items">
+              {pastry.map((recip, index) => (
+                <Recipe
+                  key={index}
+                  label={recip.recipe.label}
+                  image={recip.recipe.image}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="all-item-wrapper">
+            <span>PASTA</span>
+            <div className="items">
+              {pasta.map((recip, index) => (
+                <Recipe
+                  key={index}
+                  label={recip.recipe.label}
+                  image={recip.recipe.image}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="all-item-wrapper">
+            <span>SALAD</span>
+            <div className="items">
+              {salad.map((recip, index) => (
+                <Recipe
+                  key={index}
+                  label={recip.recipe.label}
+                  image={recip.recipe.image}
+                />
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="all-item-wrapper">
-        <span>Chicken</span>
-        <div className="items">
-          {chicken.map((recip, index) => (
-            <Recipe
-              key={index}
-              label={recip.recipe.label}
-              image={recip.recipe.image}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="all-item-wrapper">
-        <span>PASTRY</span>
-        <div className="items">
-          {pastry.map((recip, index) => (
-            <Recipe
-              key={index}
-              label={recip.recipe.label}
-              image={recip.recipe.image}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="all-item-wrapper">
-        <span>PASTA</span>
-        <div className="items">
-          {pasta.map((recip, index) => (
-            <Recipe
-              key={index}
-              label={recip.recipe.label}
-              image={recip.recipe.image}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="all-item-wrapper">
-        <span>SALAD</span>
-        <div className="items">
-          {salad.map((recip, index) => (
-            <Recipe
-              key={index}
-              label={recip.recipe.label}
-              image={recip.recipe.image}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import react from "react";
 import FetchRecipe from "../components/common/FetchRecipie";
 import Slider from "../components/Slider/Slider";
+
 import Image from "../components/assets/Ellipse 10.png";
 
 import "../styles/Home.css";
@@ -11,6 +12,7 @@ const Home = () => {
       <div className="slider-con">
         <Slider />
       </div>
+
       <FetchRecipe />
     </div>
   );
